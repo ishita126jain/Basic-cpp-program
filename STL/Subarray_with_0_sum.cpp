4a1e983384432ec6dbd9cb764f81fb78e4cbdce9
IshitaJain <91709949+ishita126jain@users.noreply.github.com>
2022-09-04 17:46:05 +0530

    Create Subarray_with_0_sum.cpp

---
+++ STL/Subarray_with_0_sum.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    //Complete this function
+    //Function to check whether there is a subarray present with 0-sum or not.
+    bool subArrayExists(int arr[], int n)
+    {
+        //Your code here
+        unordered_set<int> s;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+            if(sum==0 || s.find(sum)!=s.end()){
+                return true;
+            }
+            
+            s.insert(sum);
+        }
+        return false;
+        
+    }
+};
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++)
+	    cin>>arr[i];
+	    Solution obj;
+	    	if (obj.subArrayExists(arr, n))
+		cout << "Yes\n";
+	else
+		cout << "No\n";
+	}
+	return 0;
+}
